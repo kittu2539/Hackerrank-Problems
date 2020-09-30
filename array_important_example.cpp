e01dfaf6e8cc1ffbe0273e1b5faea77e816e4912
Thunderbolt2-o <68735588+Thunderbolt2-o@users.noreply.github.com>
2020-09-30 19:38:03 +0530

    update3.o
    great work

--- array_important_example.cpp
+++ array_important_example.cpp
@@ -7,7 +7,7 @@ int main(void)
     int array[] = {2, 9, 8, 4, 6, 1, 7, 5, 3, 0};
     for(int i = 0; i < 10; i++)
     {
-        array[i/2] = array[array[[i/3]/3]/3];
+        array[i/2] = array[array[[i/3]/3]/3]; // great work and code
     }
     
     for(int i = 0; i < 10; i++)
